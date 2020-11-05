9dfeb95d028bde3829afe3832837a95a432d33e3
msmoni3 <msmoni3@gmail.com>
2020-11-05 11:04:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-radio-buttons-demo-reactive-forms-8tz2uu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-radio-buttons-demo-reactive-forms-8tz2uu)
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators ,FormControl } from "@angular/forms";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  isSubmitted = false;
+
+  constructor(public fb: FormBuilder) { }
+
+  /*########### Form ###########*/
+  registrationForm = this.fb.group({
+    firstName: new FormControl(''),
+    gender: ['male', [Validators.required]]
+  })
+
+  // Getter method to access form control
+  get myForm() {
+    return this.registrationForm.get('gender');
+  }
+
+  // Submit Registration Form
+  onSubmit() {
+    this.isSubmitted = true;
+    if(!this.registrationForm.valid) {
+      return false;
+    } else {
+      alert(JSON.stringify(this.registrationForm.value))
+    }
+  }  
+
+}
